03c953d6c823c4d631fa544c885c9d9b5f823409
caddu2 <133607096+caddu2@users.noreply.github.com>
2023-05-17 10:00:43 -0300

    email.php

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Dados do formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $local = $_POST['local'];
+    $mensagem = $_POST['mensagem'];
+
+    // Configurações do e-mail
+    $to = "carlosaraujo26032009@gmail.com";  // Insira o seu endereço de e-mail aqui
+    $subject = "Relato de Xenofobia";
+    $message = "Nome: " . $nome . "\n" .
+               "E-mail: " . $email . "\n" .
+               "Local onde ocorreu: " . $local . "\n" .
+               "Descrição do incidente: " . $mensagem;
+
+    // Enviar o e-mail
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Ocorreu um erro ao enviar o e-mail.";
+    }
+}
+?>
